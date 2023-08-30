7139e240f83057cca26bc9d807be215fe2ff3652
unknown <marcussalopaso1@gmail.com>
2023-08-30 11:37:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 // App.js
 import React from 'react';
 import Router from './Router';
-
 import './App.css'
-function App() {
-  window.scrollTo(0,0)
-  window.history.pushState({}, '', '/');
-  window.dispatchEvent(new PopStateEvent('popstate'));
 
+function App() {
   return (
     <div>
       <Router />
